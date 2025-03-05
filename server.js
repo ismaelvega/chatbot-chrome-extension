e0ac54d644f922b75e914629e9776dd879629694
ismaelvega <ismaelgrive@gmail.com>
2025-03-05 16:36:49 -0600

    fixed port issue

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const app = express();
 
 app.use(cors());
 
+const PORT = process.env.PORT || 3000;
+
 app.get("/", (req, res) => {
   // sends a hello world in json format
   res.json({ message: "Hello World" });
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Server is running on port 3000");
 });
